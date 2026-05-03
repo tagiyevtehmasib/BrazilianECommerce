aa9b1d7eaef942a86cd056d2e68b9ec592d261b3
tagiyevtehmasib <tagiyevtehmasib1289@gmail.com>
2026-05-03 15:19:38 +0400

    Lines came successful.

--- Data Profiling.sql
+++ Data Profiling.sql
@@ -11,3 +11,32 @@ Character_Maximum_Length
 FROM INFORMATION_SCHEMA.COLUMNS 
 WHERE TABLE_CATALOG = 'CopyBrazilianECommerce' 
 ORDER BY TABLE_NAME, ORDINAL_POSITION
+
+
+--Is the number of rows the same as in the original table?
+USE BrazilianECommerce
+
+SELECT COUNT(*) FROM category_names  -- 72
+SELECT COUNT(*) FROM customers       -- 99441
+SELECT COUNT(*) FROM geolocations    -- 1000163
+SELECT COUNT(*) FROM order_items     -- 112650
+SELECT COUNT(*) FROM order_payments  -- 103886
+SELECT COUNT(*) FROM order_reviews   -- 99224
+SELECT COUNT(*) FROM orders          -- 99441
+SELECT COUNT(*) FROM products        -- 32951
+SELECT COUNT(*) FROM sellers         -- 3095
+--=============================================================================
+
+USE CopyBrazilianECommerce
+
+SELECT COUNT(*) FROM category_names  -- 72
+SELECT COUNT(*) FROM customers       -- 99441
+SELECT COUNT(*) FROM geolocations    -- 1000163
+SELECT COUNT(*) FROM order_items     -- 112650
+SELECT COUNT(*) FROM order_payments  -- 103886
+SELECT COUNT(*) FROM order_reviews   -- 99224
+SELECT COUNT(*) FROM orders          -- 99441
+SELECT COUNT(*) FROM products        -- 32951
+SELECT COUNT(*) FROM sellers         -- 3095
+
+-- Yes, all the lines came in full.
