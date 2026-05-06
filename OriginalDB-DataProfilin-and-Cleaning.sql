638dc03d2d479dc1fbd81a137c17f2cfaf1644a1
tagiyevtehmasib <tagiyevtehmasib1289@gmail.com>
2026-05-06 16:20:05 +0400

    Change Column Name

---
+++ OriginalDB-DataProfilin-and-Cleaning.sql
@@ -0,0 +1,60 @@
+
+
+
+USE BrazilianECommerce
+
+-- All tables, their columns and their data types
+SELECT Table_Name,
+Column_Name,
+Data_Type,
+Character_Maximum_Length
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_CATALOG = 'BrazilianECommerce' 
+ORDER BY TABLE_NAME, ORDINAL_POSITION
+
+-- The copy of original database created, with .bak file.
+-- Is the number of rows the same as in the original table?
+USE BrazilianECommerce
+
+SELECT COUNT(*) FROM category_names  -- 72
+SELECT COUNT(*) FROM customers       -- 99441
+SELECT COUNT(*) FROM geolocations    -- 1000163
+SELECT COUNT(*) FROM order_items     -- 112650
+SELECT COUNT(*) FROM order_payments  -- 103886
+SELECT COUNT(*) FROM order_reviews   -- 99224
+SELECT COUNT(*) FROM orders          -- 99441
+SELECT COUNT(*) FROM products        -- 32951
+SELECT COUNT(*) FROM sellers         -- 3095
+--=============================================================================
+
+
+USE CopyBrazilianECommerce
+
+SELECT COUNT(*) FROM category_names  -- 72
+SELECT COUNT(*) FROM customers       -- 99441
+SELECT COUNT(*) FROM geolocations    -- 1000163
+SELECT COUNT(*) FROM order_items     -- 112650
+SELECT COUNT(*) FROM order_payments  -- 103886
+SELECT COUNT(*) FROM order_reviews   -- 99224
+SELECT COUNT(*) FROM orders          -- 99441
+SELECT COUNT(*) FROM products        -- 32951
+SELECT COUNT(*) FROM sellers         -- 3095
+
+-- Yes, all the lines came in full.
+--=============================================================================
+
+SELECT COUNT(*) FROM category_names
+WHERE column1 IS NOT NULL OR column1 IS NULL OR  column2 IS NOT NULL OR column2 IS NULL
+
+--=============================================================================
+-- DATA CLEANING
+-- Appoint the column names.
+
+SELECT * FROM category_names
+
+EXEC sp_rename 'category_names.column1', 'category_portugal'
+EXEC sp_rename 'category_names.column2', 'category_english'
+
+--Delete the first row, because previous column names were there.
+DELETE FROM category_names
+WHERE category_portugal = 'product_category_name'
